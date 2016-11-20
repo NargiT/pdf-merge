767dda832a834bdc6e07bf8291f968410b14500c
Tigran Tch <nargitinthenight@gmail.com>
2016-11-20 01:01:02 +0100

    add VB8 impl

---
+++ pdf-mergeVB/Module1.vb
@@ -0,0 +1,23 @@
+﻿Imports iText.Forms
+Imports iText.Kernel.Pdf
+Imports iText.Pdfa
+
+Module Module1
+
+    Sub Main()
+        Dim pdf1 = New PdfDocument(New PdfReader("path"))
+        Dim pdf2 = New PdfDocument(New PdfReader("path"))
+
+        Dim pdfWriter = New PdfWriter("finale")
+        pdfWriter.SetSmartMode(True)
+        Dim finalPdf = New PdfDocument(pdfWriter)
+        finalPdf.InitializeOutlines()
+
+        pdf1.CopyPagesTo(1, pdf1.GetNumberOfPages(), finalPdf, New PdfPageFormCopier())
+        pdf2.CopyPagesTo(1, pdf2.GetNumberOfPages(), finalPdf, New PdfPageFormCopier())
+        finalPdf.Close()
+    End Sub
+
+
+
+End Module
